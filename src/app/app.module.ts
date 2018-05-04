4cdbe6f5508cb1f3741b7d03f85758fc1199cf02
jabari <jiangzg@walnut.im>
2018-05-04 18:12:27 +0800

    study

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,24 @@ import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import {HeroService} from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [
-  HeroService
+  HeroService,
+  MessageService
   ],
   bootstrap: [AppComponent]
 })
